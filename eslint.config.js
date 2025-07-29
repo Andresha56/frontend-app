2edd165fb67687748d048a0547d65564c3669f12
rinki <rinki@unifylist.com>
2025-07-29 21:42:47 +0530

    feat- added tyescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import pluginPrettier from "eslint-plugin-prettier";
 import pluginA11y from "eslint-plugin-jsx-a11y";
@@ -7,7 +8,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,jsx,mjs,cjs}"],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     languageOptions: {
       parserOptions: {
         ecmaVersion: "latest",
@@ -31,12 +32,14 @@ export default defineConfig([
     rules: {
       ...js.configs.recommended.rules,
       ...pluginReact.configs.flat.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+
       "prettier/prettier": "error",
+      "no-console": "error",
+      "no-debugger": "error",
       "react/react-in-jsx-scope": "off",
       "react/jsx-no-comment-textnodes": "off",
       "react/prop-types": "off",
-      "no-console": "error",
-      "no-debugger": "error",
 
       "jsx-a11y/anchor-is-valid": [
         "error",
